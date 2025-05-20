2d80a1159f43970c960c2eb8e1ff4c9adf60d336
Sandip Thapa <sandipthapa386@gmail.com>
2025-05-20 14:13:07 +0545

    fix: Handling zod validation error in error-handlers

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import { ErrorCode, HttpException } from './exceptions/root';
 import { InnternalException } from './exceptions/internal-exception';
+import { ZodError } from 'zod';
+import { BadRequestException } from './exceptions/bad-request';
 
 export const errorHandler = (method: Function) => {
   return async (req: Request, res: Response, next: NextFunction) => {
@@ -11,11 +13,18 @@ export const errorHandler = (method: Function) => {
       if (error instanceof HttpException) {
         exception = error;
       } else {
-        exception = new InnternalException(
-          'Something went wrong',
-          error,
-          ErrorCode.INTERNAL_EXCEPTION
-        );
+        if (error instanceof ZodError) {
+          exception = new BadRequestException(
+            'Unprocessable entity',
+            ErrorCode.UNPROCESSABLE_ENTITY
+          );
+        } else {
+          exception = new InnternalException(
+            'Something went wrong',
+            error,
+            ErrorCode.INTERNAL_EXCEPTION
+          );
+        }
       }
       next(exception);
     }
